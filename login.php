c075c1907a6bde0431681de067bd7bdfe4acca64
Joao Pedro dos Santos Alves <163792292+JoaoPedroDosSantosAlves@users.noreply.github.com>
2024-05-21 23:14:13 -0300

    login.php

--- login.php
+++ login.php
@@ -2,8 +2,10 @@
 session_start();
 
 if ($_POST['email'] === 'usuario@example.com' && $_POST['password'] === '123456') {
-    $_SESSION['username'] = 'usuario'; // Criar a sessão
-    setcookie('username', 'usuario', time() + (86400 * 30), "/"); // Criar o cookie com o nome do usuário (válido por 30 dias)
+    // Criar a sessão
+    $_SESSION['username'] = 'usuario'; 
+    // Criar o cookie
+    setcookie('username', 'usuario', time() + (86400 * 30), "/"); 
     header("Location: content.php");
     exit();
 } else {
